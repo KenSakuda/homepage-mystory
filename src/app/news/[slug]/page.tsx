5e063ab4af71170779c15a3d927fb5ca7cb81c6b
KenSakuda <ken_sakuda@b-mystory.com>
2025-01-15 01:37:56 +0900

    ver.1.1をコミット

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,25 +1,70 @@
+// import type { Metadata } from "next";
+// import { notFound } from "next/navigation";
+// import { getNewsDetail } from "@/app/_libs/microcms";
+// import Article from "@/app/_components/Article";
+// import ButtonLink from "@/app/_components/ButtonLink";
+// import styles from "./page.module.css";
+
+// type Props = {
+//   params: {
+//     slug: string;
+//   };
+//   searchParams: {
+//     dk?: string;
+//   };
+// };
+
+// export async function generateMetadata({
+//   params,
+//   searchParams,
+// }: Props): Promise<Metadata> {
+//   const data = await getNewsDetail(params.slug, { draftKey: searchParams.dk });
+
+//   return {
+//     title: data.title,
+//     openGraph: {
+//       title: data.title,
+//     },
+//   };
+// }
+
+// export default async function Page({ params, searchParams }: Props) {
+//   const data = await getNewsDetail(params.slug, {
+//     draftKey: searchParams.dk,
+//   }).catch(notFound);
+
+//   return (
+//     <>
+//       <Article data={data} />
+//       <div className={styles.button}>
+//         <ButtonLink href="/news">お知らせ一覧へ戻る</ButtonLink>
+//       </div>
+//     </>
+//   );
+// }
+
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
-
-type Props = {
+// 型を定義する
+type PageProps = {
   params: {
-    slug: string;
+    slug: string; // 必須
   };
-  searchParams: {
-    dk?: string;
+  searchParams?: {
+    dk?: string; // オプション型
   };
 };
-
+// generateMetadata 関数
 export async function generateMetadata({
   params,
-  searchParams,
-}: Props): Promise<Metadata> {
-  const data = await getNewsDetail(params.slug, { draftKey: searchParams.dk });
-
+}: PageProps): Promise<Metadata> {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: "",
+  });
   return {
     title: data.title,
     openGraph: {
@@ -27,12 +72,13 @@ export async function generateMetadata({
     },
   };
 }
-
-export default async function Page({ params, searchParams }: Props) {
+// ページコンポーネント
+export default async function Page({ params, searchParams }: PageProps) {
   const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams.dk,
-  }).catch(notFound);
-
+    draftKey: searchParams?.dk ?? "", // オプションで存在しない場合は空文字を使用
+  }).catch(() => {
+    return notFound();
+  });
   return (
     <>
       <Article data={data} />
