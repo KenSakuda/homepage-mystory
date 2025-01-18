f6afb6fcf47ed4be79a8ce325b2031a89f1bac8b
KenSakuda <ken_sakuda@b-mystory.com>
2025-01-18 18:09:25 +0900

    v1.19をコミット

--- src/app/_components/ServicesSection/index.tsx
+++ src/app/_components/ServicesSection/index.tsx
@@ -61,7 +61,7 @@ export default function ServicesSection() {
         </div>
         <div className={styles.card}>
           <Image
-            src="/photo_service_image4.jpg"
+            src="/photo_service_image4.png"
             alt="ウエルネス関連のサービス開発"
             width={350}
             height={220}
