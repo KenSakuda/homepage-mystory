e05eaca7273c79e87060953813a13b32e9db3019
KenSakuda <ken_sakuda@b-mystory.com>
2025-03-14 21:14:39 +0900

    ver3.3 commit

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -17,17 +17,34 @@ export default function Page() {
         </div>
         <div className={styles.infoCard}>
           <h2>事業内容</h2>
-          <p>
-            データ分析コンサルティング、ブロックチェーン開発、コンテンツ制作・デジタルメディアの運営など
-          </p>
+          <ul>
+            <li>データ分析コンサルティング</li>
+            <li>データ分析ツールの開発、並びに販売</li>
+            <li>スポーツニュースサイト「Deep Sports」の運営</li>
+            <li>
+              自社開発マーケットプレイスの運営、並びにブロックチェーン受託開発
+            </li>
+            <li>ヘルスケア関連アプリの企画・開発</li>
+          </ul>
+        </div>
+        <div className={styles.infoCard}>
+          <h2>所在地</h2>
+          <p>東京都港区南青山二丁目2番15号 ウィン青山942</p>
+        </div>
+        <div className={styles.infoCard}>
+          <h2>従事者数</h2>
+          <p>9名 ※正社員以外も含む</p>
         </div>
         <div className={styles.infoCard}>
           <h2>資本金</h2>
           <p>3,000,000円</p>
         </div>
         <div className={styles.infoCard}>
-          <h2>所在地</h2>
-          <p>東京都港区南青山二丁目2番15号 ウィン青山942</p>
+          <h2>加盟学会</h2>
+          <p>
+            日本行動計量学会、日本消費者行動研究学会、日本統計学会
+            スポーツデータサイエンス分科会
+          </p>
         </div>
         <div className={styles.infoCard}>
           <h2>プライバシーポリシー</h2>
