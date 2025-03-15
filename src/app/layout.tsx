b0f18dc01531ea1287b173204fc675d9c618a36a
KenSakuda <ken_sakuda@b-mystory.com>
2025-03-15 15:19:44 +0900

    ver3.4 commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https:/www./b-mystory.com"),
+  metadataBase: new URL("https:/www.b-mystory.com"),
   title: {
     template: "%s | データ分析・経済学のビジネス活用の相談ならMyStory",
     default: "データ分析・経済学のビジネス活用の相談ならMyStory",
@@ -13,8 +13,10 @@ export const metadata: Metadata = {
   description: "先端技術を活用して、あらゆるビジネス課題を解決します",
   openGraph: {
     title: "MyStory",
+    siteName: "MyStory",
     description:
       "AIやブロックチェーン、モダンアプリ開発など先端技術の力を駆使して、あらゆるビジネス課題を解決します",
+    type: "website",
   },
 };
 
@@ -25,6 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="nTKt9Ds83hQ8GyrbKfRYlKHNTrBWzWUbMmDlGUyN4ZU"
+        />
+      </head>
       <body>
         <Header />
         {children}
