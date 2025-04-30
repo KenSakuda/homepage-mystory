8dd96f46f43684ea193be01bbebabafdbac9e91d
KenSakuda <ken_sakuda@b-mystory.com>
2025-04-30 20:14:16 +0900

    v4.8 commit

--- src/app/_components/Header/HamburgerButton.tsx
+++ src/app/_components/Header/HamburgerButton.tsx
@@ -1,26 +1,49 @@
-"use client";
+// "use client";
 
+// import styles from "./index.module.css";
+// import Image from "next/image";
+
+// type Props = {
+//   open: boolean;
+//   setOpen: (open: boolean) => void;
+// };
+
+// export default function HamburgerButton({ open, setOpen }: Props) {
+//   return (
+//     <button
+//       className={styles.hamburgerButton}
+//       onClick={() => setOpen(!open)}
+//       aria-label="メニュー"
+//     >
+//       {open ? (
+//         <Image src="/close.svg" alt="閉じる" width={24} height={24} />
+//       ) : (
+//         <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+//       )}
+//     </button>
+//   );
+// }
+
+"use client";
 import styles from "./index.module.css";
 import Image from "next/image";
-
 type Props = {
   open: boolean;
   setOpen: (open: boolean) => void;
 };
-
 export default function HamburgerButton({ open, setOpen }: Props) {
   return (
     <button
-      className={styles.hamburgerButton}
+      className={styles.hamburgerFixedButton}
       onClick={() => setOpen(!open)}
       aria-label="メニュー"
     >
-      {/* {open ? ":x_黒太字:" : "☰"} */}
-      {open ? (
-        <Image src="/close.svg" alt="閉じる" width={24} height={24} />
-      ) : (
-        <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
-      )}
+      <Image
+        src={open ? "/close.svg" : "/menu.svg"}
+        alt={open ? "閉じる" : "メニュー"}
+        width={24}
+        height={24}
+      />
     </button>
   );
 }
